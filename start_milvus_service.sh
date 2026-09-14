9c002cbe1e5057caf8c8988222945a6ca007c9f9
Lauren Chilutti <lauren.chilutti@noaa.gov>
2026-09-14 13:12:46 -0400

    add replace flag to the milvus container run

--- start_milvus_service.sh
+++ start_milvus_service.sh
@@ -12,6 +12,7 @@ touch -a user.yaml
 
 podman run -d \
         --name milvus-standalone \
+	--replace \
         --security-opt seccomp:unconfined \
         -e ETCD_USE_EMBED=true \
         -e ETCD_DATA_DIR=/var/lib/milvus/etcd \
